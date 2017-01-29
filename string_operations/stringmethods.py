dbc20767cdb920d43c50181fd6ec66249f283ea5
LarsBergqvist <lars.g.bergqvist@gmail.com>
2017-01-29 20:59:37 +0100

    string method examples with doctest

---
+++ string_operations/stringmethods.py
@@ -0,0 +1,54 @@
+"""
+>>> 'hello friend'.capitalize()
+'Hello friend'
+
+>>> 'Hello friend'.center(20,'*')
+'****Hello friend****'
+
+>>> 'Hello friend'.center(10,'*')
+'Hello friend'
+
+>>> 'hello friend hello'.count('hello',0,20)
+2
+
+>>> 'hello friend hello'.count('hello',0,10)
+1
+
+>>> b'hello friend'.decode('UTF-8')
+'hello friend'
+
+>>> 'hello friend'.encode('ascii')
+b'hello friend'
+
+>>> 'hello friend'.find('hello')
+0
+
+>>> 'hello friend'.endswith('friend')
+True
+
+>>> 'hello {0} {1}'.format('again','friend')
+'hello again friend'
+
+>>> 'ABC123abc'.isalnum()
+True
+
+>>> 'ABCabc'.isalpha()
+True
+
+>>> '123'.isdigit()
+True
+
+>>> 'abc'.islower()
+True
+
+>>> 'ABC'.isupper()
+True
+
+>>> ' '.join(['word1','word2'])
+'word1 word2'
+
+"""
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
