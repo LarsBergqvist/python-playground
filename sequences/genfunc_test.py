1ce89cda5a66a46c998b3d5f42ab7f8f27122d34
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2018-02-15 09:48:20 +0100

    Added generator function example

---
+++ sequences/genfunc_test.py
@@ -0,0 +1,21 @@
+
+
+"""
+>>> for i in fibonacci(6): print(i)
+1
+1
+2
+3
+5
+8
+"""
+
+def fibonacci(n):
+   a = b = 1
+   for _ in range(n):
+      yield a
+      a, b = b, a + b
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
