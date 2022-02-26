ad7ed9e9c9f86bb59c38d1762bc9192d8fbcc841
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2022-02-26 17:57:05 +0100

    Fixed linting warnings

--- itertools_test/cycle_and_zip.py
+++ itertools_test/cycle_and_zip.py
@@ -5,26 +5,28 @@
 # are the combination of the inputs at position i. Stops at the
 # length of the shortest input iterable.
 
-x=[(1,2),2,3]
-y=[4,5,6,7]
+x = [(1, 2), 2, 3]
+y = [4, 5, 6, 7]
 # This returns an iterator of tuples
-z=zip(x,y)
+z = zip(x, y)
 print('With zip():')
 print(list(z))
 
 # itertools.zip_longest() stops at the length of the
 # longest input iterable.
 from itertools import zip_longest
-z=zip_longest(x,y)
+
+z = zip_longest(x, y)
 print('With itertools.zip_longest:')
 print(list(z))
 
 # cycle() returns an infinite iterator based on a finite iterable
 from itertools import cycle
-mylist=[1,2,3,4]
-iterator=cycle(mylist)
+
+mylist = [1, 2, 3, 4]
+iterator = cycle(mylist)
 # Though mylist is only 4 items, we can
 # iterate on cycle(mylist) forever
 # Try looping for a longer range than the length of mylist
-for i in range(0,20):
-  print(next(iterator))
+for i in range(0, 20):
+    print(next(iterator))
